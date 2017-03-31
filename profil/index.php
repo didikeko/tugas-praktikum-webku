f9668f757e515459c8a1c49aea499588fb66d2f1
didikeko <didikeko1997@gmail.com>
2017-03-31 11:43:02 +0700

    profile web
    profile web

---
+++ profil/index.php
@@ -0,0 +1,34 @@
+<?php require "header.php" ?>
+
+<h3 align="center">WELCOME TO MY HOUSE</h3>
+
+<table>
+
+    <tr>
+        <td>Nama  : </td>
+        <td>Didik Eko Pramono</td>
+    </tr>
+    <tr>
+        <td>Alamat : </td>
+        <td>Konohagukure</td>
+    </tr>
+    <tr>
+        <td>Tempat Lahir :</td>
+        <td>Wonogiri</td>
+    </tr>
+    <tr>
+        <td>Hobi :</td>
+        <td>Pencilaan , fly</td>
+    </tr>
+    <tr>
+        <td>Kata Mutiara : </td>
+        <td>Hidup ini tak lebih dari telur</td>
+    </tr>
+    <tr>
+        <td>Pekerjaan : </td>
+        <td>Hokage Konoha</td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ profil/sosialmedia.php
@@ -0,0 +1,28 @@
+<?php require "header.php" ?>
+<table>
+
+    <tr>
+        <td>Fb :</td>
+        <td>Didiex_eko@ymail.com</td>
+    </tr>
+    <tr>
+        <td>IG :</td>
+        <td>Tx_Anomic</td>
+    </tr>
+    <tr>
+        <td>Whatssap :</td>
+        <td>082236805438</td>
+    </tr>
+    <tr>
+        <td>Pin :</td>
+        <td>hbdhb788</td>
+    </tr>
+    <tr>
+        <td>Email :</td>
+        <td>Didikeko1997@gmail.com</td>
+    </tr>
+    <tr>
+        <td>Telepon : </td>
+        <td>(0273)-6679990</td>
+    </tr>
+</table>
